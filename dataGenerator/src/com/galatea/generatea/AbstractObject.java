0546fb794149a1c76e0a19d3bba5ee4f024924bd
Janaki <janakihere@gmail.com>
2014-01-28 10:23:58 -0500

    commit and push

--- dataGenerator/src/com/galatea/generatea/AbstractObject.java
+++ dataGenerator/src/com/galatea/generatea/AbstractObject.java
@@ -1,84 +1,89 @@
 package com.galatea.generatea;
+
 import java.util.ArrayList;
 import java.util.List;
 
-public class AbstractObject{
-  ArrayList<Attribute> attributes;
-  private int numInstances;
-  List<AbstractObject> children;
-  String name;
-  
-  public AbstractObject(String n, ArrayList<Attribute> a,int numOfInstances,ArrayList<AbstractObject> ch){
-    name = n;
-	attributes = a;
-	children = ch;
-	numInstances = numOfInstances;
-  }
-  
-  public AbstractObject(){
-    name = "default";
-	attributes = new ArrayList();
-	children = new ArrayList();
-	numInstances = 0;
-  }
-  
-  public AbstractObject(String n,int numOfInstances){
-    name = n;
-	attributes = new ArrayList();
-	children = new ArrayList();
-	numOfInstances = 0;
-  }
-  
-  public AbstractObject(ArrayList<Attribute> a,int numOfInstances,ArrayList<AbstractObject> ch){
-    name = "default";
-	attributes = a;
-	children = ch;
-	numInstances = numOfInstances;
-  }
-  
-  public void setName(String n){
-    name = n;
-  }
-  
-  public void setAttributes(ArrayList<Attribute> a){
-    attributes = a;
-  }
-  
-  public void clearAttributes(){
-    attributes.clear();
-    numInstances = 0;
-  }
-  
-  public void addAttribute(Attribute a){
-    attributes.add(a);
-  }
-  
-  public List<AbstractObject> getChildren() {
-	return children;
-}
+public class AbstractObject {
 
-public void setChildren(List<AbstractObject> children) {
-	this.children = children;
-}
-public void addAChild(AbstractObject c){
-    children.add(c);
-  }
+	private ArrayList<Attribute> attributes;
+	private int numInstances;
+	private List<AbstractObject> children;
+	private String name;
 
-public int getNumInstances() {
-	return numInstances;
-}
-
-public void setNumInstances(int numInstances) {
-	this.numInstances = numInstances;
-}
-
-public String toString(){
-    StringBuilder sb = new StringBuilder(name +  "(AbstractObject): \n");
-	for(Attribute a : attributes){
-	  sb.append("\t");
-	  sb.append(a.toString());
+	public AbstractObject(String n, ArrayList<Attribute> a, int numOfInstances,
+			ArrayList<AbstractObject> ch) {
+		name = n;
+		attributes = a;
+		children = ch;
+		numInstances = numOfInstances;
 	}
-	
-	return sb.toString();
-  }
+
+	public AbstractObject() {
+		name = "default";
+		attributes = new ArrayList();
+		children = new ArrayList();
+		numInstances = 0;
+	}
+
+	public AbstractObject(String n, int numOfInstances) {
+		name = n;
+		attributes = new ArrayList();
+		children = new ArrayList();
+		numOfInstances = 0;
+	}
+
+	public AbstractObject(ArrayList<Attribute> a, int numOfInstances,
+			ArrayList<AbstractObject> ch) {
+		name = "default";
+		attributes = a;
+		children = ch;
+		numInstances = numOfInstances;
+	}
+
+	public void setName(String n) {
+		name = n;
+	}
+
+	public void setAttributes(ArrayList<Attribute> a) {
+		attributes = a;
+	}
+
+	public void clearAttributes() {
+		attributes.clear();
+		numInstances = 0;
+	}
+
+	public void addAttribute(Attribute a) {
+		attributes.add(a);
+	}
+
+	public List<AbstractObject> getChildren() {
+		return children;
+	}
+
+	public void setChildren(List<AbstractObject> children) {
+		this.children = children;
+	}
+
+	public void addAChild(AbstractObject c) {
+		children.add(c);
+	}
+
+	public int getNumInstances() {
+		return numInstances;
+	}
+
+	public void setNumInstances(int numInstances) {
+		this.numInstances = numInstances;
+	}
+
+	public String toString() {
+		StringBuilder sb = new StringBuilder(name + "(AbstractObject): \n");
+		for (Attribute a : attributes) {
+			sb.append("\t");
+			sb.append(a.toString());
+		}
+
+		return sb.toString();
+	}
 }
