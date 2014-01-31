ae0141bb95bca96d3f53ea73488deaebb226ce1d
Janaki <janakihere@gmail.com>
2014-01-31 13:36:39 -0500

    Manager added

---
+++ dataGenerator/src/com/galatea/generatea/GeneratorManager.java
@@ -0,0 +1,17 @@
+package com.galatea.generatea;
+
+import java.util.List;
+
+import com.galatea.generatea.Tables.AttributeValue.AttributeVal;
+import com.google.common.collect.HashBasedTable;
+
+public class GeneratorManager {
+
+	// Table will sit here
+	AbstractObject parentObjest;
+	HashBasedTable<String, String, AttributeVal> table;
+	
+	public void manageAttributeGeneration(){
+		
+	}
+}
