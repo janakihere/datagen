9f6ca0e2d3aebacc929b9da517f9fde1999609d2
Janaki <janakihere@gmail.com>
2014-01-31 13:37:00 -0500

    String Generator

---
+++ dataGenerator/src/com/galatea/generatea/Generators/RegexStringGenerator.java
@@ -0,0 +1,31 @@
+package com.galatea.generatea.Generators;
+
+import nl.flotsam.xeger.Xeger;
+
+import com.galatea.generatea.Tables.AttributeValue.AttributeVal;
+
+public class RegexStringGenerator extends Generator<String> {
+
+	String regex;
+
+	public RegexStringGenerator(String regex) {
+		super();
+		this.regex = regex;
+	}
+
+	@Override
+	public String generate() {
+		String result = "";
+		Xeger generator = new Xeger(this.regex);
+		result = generator.generate();
+		// System.out.println(result);
+		return result;
+	}
+
+	@Override
+	public AttributeVal<String> generateAttributeVal() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
