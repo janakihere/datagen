958f1b603d6c243733596e6b169c747847a2554b
Janaki <janakihere@gmail.com>
2014-02-07 10:40:24 -0500

    table referencing

--- dataGenerator/src/com/galatea/generatea/GeneratorManager.java
+++ dataGenerator/src/com/galatea/generatea/GeneratorManager.java
@@ -12,46 +12,48 @@ import javax.xml.parsers.ParserConfigurationException;
 
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
+import org.w3c.dom.NamedNodeMap;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 
+import com.galatea.generatea.Generators.Generator;
 import com.galatea.generatea.Tables.AttributeValue.AttributeVal;
+import com.galatea.generatea.Tables.AttributeValue.TableVal;
 import com.google.common.collect.HashBasedTable;
 
 public class GeneratorManager {
 
 	// Table will sit here
 	HashBasedTable<String, String, AttributeVal> table;
-	
+	AttributeVal<AbstractObject> a ;
 	public void manageAttributeGeneration(AbstractObject parent){
 		//for each child generte the children
-		if(parent.getChildren().size() != 0){
-			generateChildren(parent);
-		}
+		//alternate 
+		generateTree(parent);
+		
 		
 	}
 	
-	private void generateAttributes(List<Attribute> attr,int numOfTimes){
-		// for each attribute get the dependant and genrate into table.
-		/**for(){
-			if dependant on is not empty
-			get dependeant
-			generate
-			table
-			
-			
-		}**/
-	}
-	private void generateChildren(AbstractObject obj){
-		for(AbstractObject c:obj.getChildren()){
-			if(c.getChildren().size() != 0){
-				generateChildren(c);
+	private HashBasedTable<Integer, String, AttributeVal> generateTree(AbstractObject obj){
+		HashBasedTable<Integer, String, AttributeVal> table = null;
+		HashBasedTable<Integer, String, AttributeVal> childTable = null;
+		for(int i=0;i<obj.getNumInstances();i++){
+			for(Attribute at: obj.getAttributes()){
+				Generator g = 	at.Generator();
+				AttributeVal a = g.generateAttributeVal();
+				table.put(i, at.getName(), a);
 			}
-			else{
-				generateAttributes(obj.getAttributes(),obj.getNumInstances());
+			if(obj.getChildren().size() != 0){
+				for(AbstractObject child: obj.getChildren()){
+					childTable =  generateTree(child);
+					//table.put(i,child.getName(),childAttr);
+				}
 			}
+			AttributeVal<HashBasedTable> childAttr = new TableVal(childTable);
+			
 		}
+		return table;	
 	}
 	public void genereteFromXSD(){
 		DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
@@ -59,8 +61,8 @@ public class GeneratorManager {
 		try {
 			db = dbf.newDocumentBuilder();
 			Document doc = db.parse(new File("C:/Users/Janaki/git/datagen_final/dataGenerator/src/com/galatea/generatea/config.xml"));
-			 List<AbstractObject> listObjs = new ArrayList<AbstractObject>();
-		     AbstractObject newObj = new AbstractObject();
+			List<AbstractObject> listObjs = new ArrayList<AbstractObject>();
+		    AbstractObject newObj = new AbstractObject();
 			System.out.println(doc.getDocumentElement().getNodeName());
 			newObj.setName(doc.getDocumentElement().getNodeName());
 
@@ -75,19 +77,36 @@ public class GeneratorManager {
                 if(mainObject.getNodeType() == Node.ELEMENT_NODE){
 
                 	Element mainObjectElement = (Element)mainObject;
-                	System.out.println(mainObjectElement.getNodeName());
+                	System.out.println("---"+mainObjectElement.getNodeName());
                     NodeList nodeListChildren = mainObjectElement.getChildNodes();
                     for(int j=0;j<nodeListChildren.getLength();j++){
                     	 if(nodeListChildren.item(j).getNodeType() == Node.ELEMENT_NODE 
                     			 && nodeListChildren.item(j).hasChildNodes()){
-                    		 System.out.println(nodeListChildren.item(j).getNodeName());
+                    		 newObj.setName(nodeListChildren.item(j).getNodeName());
+                    		 Node n = nodeListChildren.item(j);
+                    		 NamedNodeMap m = n.getAttributes();
+                    		 newObj.setNumInstances(Integer.parseInt(m.getNamedItem("maxOccurs").getNodeValue()));
+                    		 System.out.println(m.getNamedItem("maxOccurs").getNodeValue());
+                    		 System.out.println("===="+nodeListChildren.item(j).getNodeName());
                     		 Element eachObjectElement = (Element)nodeListChildren.item(j);
                     		// eachObjectElement.get
                              NodeList eachObjChildren = eachObjectElement.getChildNodes();
                     		 for(int k =0;k<eachObjChildren.getLength();k++){
                     			// System.out.println(eachObjChildren.item(k).getNodeType());
-                    			 if(eachObjChildren.item(k).getNodeType() == Node.ELEMENT_NODE ){
-                    				 System.out.println(eachObjChildren.item(k).getNodeName());
+                    			 if(eachObjChildren.item(k).getNodeType() == Node.ELEMENT_NODE && !eachObjChildren.item(k).hasChildNodes() ){
+                    				 //System.out.println(eachObjChildren.item(k).getNodeName());
+                    				 Node attr = eachObjChildren.item(k);
+//                    				 if(attr.getNodeName() == "String"){
+//                    					 Attribute a = new Attribute();
+//                    					 a.setType(Type.String);
+//                    					 a.setRegexp(attr.getAttributes().getNamedItem("regex"));
+//                    					 a.setlen(attr.getAttributes().getNamedItem("lengthOfString"));
+//                    				 } else if(attr.getNodeName() == "int"){
+//                    					 
+//                    				 }else if(attr.getNodeName() == "BigDecimal"){
+//                    					 
+//                    				 }
+                    				 
                     			 }
                     		 }
                     	 }
@@ -110,6 +129,10 @@ public class GeneratorManager {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} 
+		
+	}
+	
+	public void getAttributes(AbstractObject obj){
 		
 	}
 	public static void main(String args[]){
